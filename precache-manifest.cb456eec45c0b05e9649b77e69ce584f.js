50d443d5e0a4788c8025b5f026724d641ab26e8a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-22 09:08:53 +0000

    Deploy cdc.pycon.tw to github.com/pycontw/CCIP-HDP.git:gh-pages

---
+++ precache-manifest.cb456eec45c0b05e9649b77e69ce584f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce88ac48e1f0f1b1de38",
+    "url": "/css/app.160beac7.css"
+  },
+  {
+    "revision": "c27013b4fd50412baf2f",
+    "url": "/css/chunk-vendors.5e44303a.css"
+  },
+  {
+    "revision": "861b98e81ae13d22437a666b2b7a3fbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce88ac48e1f0f1b1de38",
+    "url": "/js/app.b1902f5f.js"
+  },
+  {
+    "revision": "c27013b4fd50412baf2f",
+    "url": "/js/chunk-vendors.db9426a7.js"
+  },
+  {
+    "revision": "9894a2de7c132bb28bda",
+    "url": "/js/declaration.53d5c413.js"
+  },
+  {
+    "revision": "d5135c25933c563f9dee",
+    "url": "/js/fill.6327b07c.js"
+  },
+  {
+    "revision": "d953b5dffd0a087c2687",
+    "url": "/js/status.0cfb137c.js"
+  },
+  {
+    "revision": "28bf2ab99af2d0770d51a57d31b2f113",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
